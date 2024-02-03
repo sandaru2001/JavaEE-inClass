c5835e9f2040860ee44b72a716b3c85edf1830f4
Sandaru Shehan <sandarushehan96@gmail.com>
2024-02-03 11:46:39 +0530

    update

---
+++ src/main/java/com/example/javaeeinclass/Values.java
@@ -0,0 +1,30 @@
+package com.example.javaeeinclass;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Values extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("customAPI");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        if (req.getContentType() == null || !req.getContentType().toLowerCase().startsWith("application/json")) {
+            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }else {
+            var writer = resp.getWriter();
+            resp.setContentType("text/html");
+
+            System.out.println("Hello " + name);
+            writer.println("Hello " + name);
+        }
+    }
+}
